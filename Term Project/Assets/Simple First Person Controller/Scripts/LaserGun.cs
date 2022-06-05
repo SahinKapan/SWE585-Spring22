51f29704fae384fa5e61bf718bb46921e99f50e7
SahinKapan <sahinkapan2635@gmail.com>
2022-06-05 22:45:19 +0300

    Grup arkadaşım Mehmet Ali Özcan'la birlikte term project yapıldı.

---
+++ Term Project/Assets/Simple First Person Controller/Scripts/LaserGun.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class LaserGun : NetworkBehaviour
+{
+    public Transform laserTransform;
+    public LineRenderer line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer && Input.GetMouseButtonDown(0))
+        {
+            //we want to shoot!
+            CmdShoot();
+        }
+    }
+
+    [Command]
+    public void CmdShoot()
+    {
+        //do a raycast to see where we hit
+        Ray ray = new Ray(laserTransform.position, laserTransform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f))
+        {
+            //we hit something- draw the line
+            //check if its a player
+            var playerHealth = hit.collider.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth)
+            {
+                //that means it's a player- respawn them
+                playerHealth.Damage(10);
+            }
+
+            RpcDrawLaser(laserTransform.position, hit.point);
+        }
+        else
+        {
+            RpcDrawLaser(laserTransform.position, laserTransform.position + laserTransform.forward * 100f);
+        }
+    }
+
+    [ClientRpc]
+    void RpcDrawLaser(Vector3 start, Vector3 end)
+    {
+        //do the laser flas enumerator function
+        StartCoroutine(LaserFlash(start, end));
+    }
+
+    IEnumerator LaserFlash(Vector3 start, Vector3 end)
+    {
+        line.SetPosition(0, start);
+        line.SetPosition(1, end);
+        yield return new WaitForSeconds(0.3f);
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, Vector3.zero);
+    }
+
+
+}
